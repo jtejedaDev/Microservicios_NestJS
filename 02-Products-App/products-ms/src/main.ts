850dabf379492e02618d7ca0d7caa6d44d6ab285
jtejeda <jtejeda@grupocomidas.com>
2024-05-29 21:03:29 +0200

    Feature Implementacion de nuestra App a microservicio

--- 02-Products-App/products-ms/src/main.ts
+++ 02-Products-App/products-ms/src/main.ts
@@ -2,12 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { env } from 'process';
 
 async function bootstrap() {
 
   const logger = new Logger('Main');
-
-  const app = await NestFactory.create(AppModule);
+  //Configuracion de nuestro App a microservcio con nestjs
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+  {
+    transport:Transport.TCP,
+    options:{
+      port: envs.port
+    }
+  }
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -17,7 +27,7 @@ async function bootstrap() {
   );
 
 
-  await app.listen(envs.port);
-  logger.log(`App running on port ${ envs.port }`);
+  await app.listen();
+  logger.log(`Products Microservice running on port ${ envs.port }`);
 }
 bootstrap();
