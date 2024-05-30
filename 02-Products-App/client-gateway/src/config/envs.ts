ec6e597a30a93b3fcd5f00e7176f4b7d80df3efa
jtejeda <jtejeda@grupocomidas.com>
2024-05-30 19:55:50 +0200

    Feature Implementacion Products Microservice y conectando al Gateway

--- 02-Products-App/client-gateway/src/config/envs.ts
+++ 02-Products-App/client-gateway/src/config/envs.ts
@@ -4,10 +4,15 @@ import * as joi from 'joi';
 
 interface EnvVars{
     PORT:number;
+    PRODUCT_MICROSERVICE_HOST:string;
+    PRODUCT_MICROSERVICE_PORT:number;
 }
 
 const envsSchema = joi.object({
-    PORT: joi.number().required()
+    PORT: joi.number().required(),
+    PRODUCT_MICROSERVICE_HOST: joi.string().required(),
+    PRODUCT_MICROSERVICE_PORT: joi.number().required(),
+
 }).unknown(true);
 
 const { error, value } = envsSchema.validate( process.env );
@@ -20,5 +25,7 @@ const envVars : EnvVars = value;
 
 export const envs = {
     port: envVars.PORT,
+    productsMicroserviceHost: envVars.PRODUCT_MICROSERVICE_HOST,
+    productsMicroservicePort: envVars.PRODUCT_MICROSERVICE_PORT
 }
 
