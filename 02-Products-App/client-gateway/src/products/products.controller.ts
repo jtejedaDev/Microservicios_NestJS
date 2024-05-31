cd92f849f035babd9ce72edb04ffce2a83a55b6c
jtejeda <jtejeda@grupocomidas.com>
2024-05-31 16:06:12 +0200

    Feature Implementacion de Manejo de excepciones en el clients_gateway

--- 02-Products-App/client-gateway/src/products/products.controller.ts
+++ 02-Products-App/client-gateway/src/products/products.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, Inject, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, Inject, Query, BadRequestException } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { PRODUCT_SERVICE } from 'src/config';
 
@@ -22,8 +23,17 @@ export class ProductsController {
   }
 
   @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: string){
-    return 'Esta funcion regresa el producto' + id
+  async findOne(@Param('id', ParseIntPipe) id: string){
+
+    try {
+      const product = await firstValueFrom(
+      this.productClient.send({ cmd: 'find_one_product' }, { id })  );
+
+      return product;
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
+
   }
 
 
