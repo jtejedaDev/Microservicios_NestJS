25e9c726e52edafd2149f9a6544f2a3c86fdc611
jtejeda <jtejeda@grupocomidas.com>
2024-06-02 16:21:49 +0200

    Feature Implementacion del microservicio de orders

--- 02-Products-App/client-gateway/src/common/dto/update-products.dto.ts
+++ 02-Products-App/client-gateway/src/common/dto/update-products.dto.ts
@@ -1,6 +1,5 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { CreateProductDto } from "./create-products.dto";
-import { IsNumber, IsPositive } from "class-validator";
 
 export class UpdateProductDto extends PartialType(CreateProductDto){
 }
--- 02-Products-App/client-gateway/src/config/envs.ts
+++ 02-Products-App/client-gateway/src/config/envs.ts
@@ -6,13 +6,16 @@ interface EnvVars{
     PORT:number;
     PRODUCT_MICROSERVICE_HOST:string;
     PRODUCT_MICROSERVICE_PORT:number;
+    ORDER_MICROSERVICE_HOST:string;
+    ORDER_MICROSERVICE_PORT:number;
 }
 
 const envsSchema = joi.object({
     PORT: joi.number().required(),
     PRODUCT_MICROSERVICE_HOST: joi.string().required(),
     PRODUCT_MICROSERVICE_PORT: joi.number().required(),
-
+    ORDER_MICROSERVICE_HOST: joi.string().required(),
+    ORDER_MICROSERVICE_PORT: joi.number().required(),
 }).unknown(true);
 
 const { error, value } = envsSchema.validate( process.env );
@@ -26,6 +29,8 @@ const envVars : EnvVars = value;
 export const envs = {
     port: envVars.PORT,
     productsMicroserviceHost: envVars.PRODUCT_MICROSERVICE_HOST,
-    productsMicroservicePort: envVars.PRODUCT_MICROSERVICE_PORT
+    productsMicroservicePort: envVars.PRODUCT_MICROSERVICE_PORT,
+    ordersMicroserviceHost: envVars.ORDER_MICROSERVICE_HOST,
+    ordersMicroservicePort: envVars.ORDER_MICROSERVICE_PORT,
 }
 
